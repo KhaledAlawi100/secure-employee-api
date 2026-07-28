8091439df89b70f2ac3b982e5ccae2827d1c72f4
KhaledAlawi100 <KhaledAlawi100@gmail.com>
2026-07-28 18:16:33 +0300

    feat: complete project initialization and database setup

---
+++ src/main/java/com/khaled/secure_employee_api/permission/entity/Permission.java
@@ -0,0 +1,23 @@
+package com.khaled.secure_employee_api.permission.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "permissions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+}
---
+++ src/main/java/com/khaled/secure_employee_api/user/entity/AppUser.java
@@ -0,0 +1,53 @@
+package com.khaled.secure_employee_api.user.entity;
+
+import com.khaled.secure_employee_api.role.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255 , unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private  boolean enabled=true;
+
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+
+
+
+
+
+
+}
