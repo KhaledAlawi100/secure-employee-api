c1e8ba0c715979409c1458e344ef0d5155a35076
KhaledAlawi100 <KhaledAlawi100@gmail.com>
2026-07-28 12:15:51 +0300

    chore: initialize secure employee api

---
+++ src/main/java/com/khaled/secure_employee_api/SecureEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.khaled.secure_employee_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureEmployeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaled/secure_employee_api/SecureEmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaled.secure_employee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureEmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
